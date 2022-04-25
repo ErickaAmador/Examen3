fa566962eb21ff06244c560b945486cc5e881337
Ericka Amador <vanessaamador830@gmail.com>
2022-04-25 17:06:57 -0600

    Examen
    Falta Login

---
+++ ProyectoExamenBlazor/Examen3/Servicios/UsuarioServicio.cs
@@ -0,0 +1,45 @@
+﻿using Blazor.Data;
+using Datos.Interfaces;
+using Datos.Repositorio;
+using Examen3.Interfaces;
+using Modelos;
+
+namespace Examen3.Servicios;
+
+public class UsuarioServicio : IUsuarioServicio
+{
+    private readonly MySQLConfiguration _configuration;
+    private IUsuarioRepositorio usuarioRepositorio;
+
+    public UsuarioServicio(MySQLConfiguration configuration)
+    {
+        _configuration = configuration;
+        usuarioRepositorio = new UsuarioRepositorio(configuration.CadenaConexion);
+    }
+
+
+    public Task<bool> Actualizar(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Eliminar(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Usuario>> GetLista()
+    {
+        return await usuarioRepositorio.GetLista();
+    }
+
+    public Task<Usuario> GetPorCodigo(string codigo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Nuevo(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+}
